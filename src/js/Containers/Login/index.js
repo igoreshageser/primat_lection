9486c2aefa19d6afa3ae21e28d905a16823d194c
email <igorluna@bigmir.net>
2017-09-24 01:05:54 +0300

    redux + doc

--- src/js/Containers/Login/index.js
+++ src/js/Containers/Login/index.js
@@ -9,20 +9,112 @@ import './style.scss'
 
 class Login extends Component {
 
+
+    /**
+     *  @desc - method do request to primat-bot and take all subj/lection
+     */
+    componentWillMount() {
+        console.log(this);
+        fetch('https://primat-bot.herokuapp.com/api/abstracts?course=2&semester=2&flow=%D0%BA%D0%B2')
+            .then(res => res.json())
+            .then(d => {
+                ::this.dataParse(d.data);
+            })
+            .catch((err) => console.error('some error :c'))
+    }
+
+    /**
+     * @param data (request data)
+     * @desc -  change state with subject
+     */
+    dataParse(data) {
+        const { take_subject } = this.props.AuthActions;
+        take_subject(data);
+    }
+
+    /**
+     * @param event
+     * @desc - do request and take telegraphURL
+     */
+    takeTelegraph(e) {
+        const lection_id = e.target.id;
+        const { take_url } = this.props.AuthActions;
+
+        fetch(`https://primat-bot.herokuapp.com/api/abstracts/${lection_id}`)
+            .then(res => res.json())
+            .then(d => {
+                let buffText = d.data.telegraph_url;
+                take_url(buffText);
+            })
+            .catch((err) => console.error('some error :c'))
+    }
+
+    /**
+     * @returns HTML with all subject
+     */
+    subjView() {
+        const subj = this.props.auth.subj;
+        if (!subj) {
+            return 'load ...'
+        } else {
+            let buff = [];
+            Object.keys(subj).map((item, index) => {
+                buff.push(
+                    <div key={index} id={item} onClick={::this.lectionView}>
+                        {item}
+                    </div>
+                )
+            })
+            return buff;
+        }
+    }
+
+    /**
+     * @param event
+     * @return HTML with lection
+     *      @redux - change state with lection
+     */
+    lectionView(e) {
+        const name = e.target.id;
+        const data = this.props.auth.subj;
+        const { take_lection } = this.props.AuthActions;
+
+        let buff = [];
+        Object.values(data).map((item) => {
+           item.map((i, itter) => {
+               if (i.subject == name) {
+                   buff.push(
+                       <div key={itter} id={i._id} onClick={::this.takeTelegraph}>
+                           {i.name}
+                       </div>
+                   )
+               }
+           })
+
+        })
+        take_lection(buff);
+    }
+
     /**
          * @desc - render component
          * @returns {HTML}
      */
     render() {
         return (
-            <h1>lel</h1>
+            <div>
+                <h5>subj</h5>
+                {::this.subjView()}
+                <hr/>
+                <h5>lections</h5>
+                {this.props.auth.lections}
+                <hr/>
+                <h5>url telegraph</h5>
+                {/*{::this.smallLectionsView()}*/}
+                {this.props.auth.telegraphUrl}
+            </div>
         )
     }
 }
-
-
-
-
 
     /**
         * @default - default give access to redux
--- src/js/REDUX/ducks/auth.js
+++ src/js/REDUX/ducks/auth.js
@@ -1,8 +1,11 @@
 /*Constants*/
 const MODAL_VIEW = 'MODAL_VIEW';
+const TAKE_LECTION = 'TAKE_LECTION';
+const TAKE_SUBJECT =  'TAKE_SUBJECT';
+const TAKE_URL = 'TAKE_URL';
+
+
 /*Actions*/
-
-
 export function modal_view(view, type) {
     return {
         type: MODAL_VIEW,
@@ -13,10 +16,35 @@ export function modal_view(view, type) {
     }
 }
 
+export function take_lection( val ) {
+    return {
+        type: TAKE_LECTION,
+        payload: val
+    }
+}
+
+export function take_subject( val ) {
+    return {
+        type: TAKE_SUBJECT,
+        payload: val
+    }
+}
+
+export function take_url( val ) {
+    return {
+        type: TAKE_URL,
+        payload: val
+    }
+}
+
+
 /*Initial State*/
 const initialState = {
     view: false,
     modalType: '',
+    lections: '',
+    subj: '',
+    telegraphUrl: ''
 };
 
 /*Reducer*/
@@ -24,6 +52,15 @@ export default function reducer(state = initialState, action) {
     switch (action.type) {
         case MODAL_VIEW:
             return {...state, view: action.payload.view, modalType: action.payload.type};
+
+        case TAKE_LECTION:
+            return {...state, lections: action.payload};
+
+        case TAKE_SUBJECT:
+            return {...state, subj: action.payload};
+
+        case TAKE_URL:
+            return {...state, telegraphUrl: action.payload};
 
         default:
             return state;
