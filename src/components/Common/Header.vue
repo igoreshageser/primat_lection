9aa1d3f981737f3d5ebae8ce9759ab439dfbd4c0
igoreshageser <igorluna@bigmir.net>
2018-07-24 22:35:24 +0300

    add avatar && username place

--- src/components/Common/Header.vue
+++ src/components/Common/Header.vue
@@ -1,10 +1,22 @@
 <template>
-  <v-toolbar class="white">
-    <v-toolbar-title @click="goToMain" class="logo">
-      <span class="blue--text lighten-2 ">KPI</span>
-      <span class="font-weight-bold">bot</span>
-    </v-toolbar-title>
-  </v-toolbar>
+  <section class="header-wrapper">
+    <v-toolbar class="white">
+      <v-toolbar-title @click="goToMain" class="logo">
+        <span class="blue--text lighten-2 ">KPI</span>
+        <span class="font-weight-bold">bot</span>
+      </v-toolbar-title>
+      <v-toolbar-items>
+        <div class="header-content">
+          <div class="username pr-3">
+            igoreshageser
+          </div>
+          <div class="avatar">
+
+          </div>
+        </div>
+      </v-toolbar-items>
+    </v-toolbar>
+  </section>
 </template>
 
 <script>
@@ -19,7 +31,24 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.logo {
-  cursor: pointer;
+.header {
+  &-wrapper {
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    .logo {
+      cursor: pointer;
+    }
+  }
+  &-content {
+    display: flex;
+    align-items: center;
+    .avatar {
+      width: 40px;
+      height: 40px;
+      background-color: blue;
+    }
+  }
 }
 </style>
