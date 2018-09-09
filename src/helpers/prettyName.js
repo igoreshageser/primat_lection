983dc90cbcdb910ed8e75dc6d55e81742b575af0
igoreshageser <igorluna@bigmir.net>
2018-09-09 14:06:10 +0300

    deploy push

---
+++ src/helpers/prettyName.js
@@ -0,0 +1,14 @@
+/**
+ * @param { String } str
+ * @returns { String }
+ */
+export function getPrettyName(str) {
+  const string = str || ''
+
+  if (string.length > 15) {
+    const subString = string.substring(0, 13)
+    return `${subString}...`
+  }
+
+  return string
+}
